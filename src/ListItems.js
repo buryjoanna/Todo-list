6f32c89d4c42c30e12606dedf9a48373719c8145
buryjoanna <buryjoanna@icloud.com>
2022-05-30 11:51:26 +0100

    checkbox icon

--- src/ListItems.js
+++ src/ListItems.js
@@ -7,9 +7,12 @@ function ListItems(props) {
   const listItems = items.map( item =>
     {
       return <div className='list' key={item.key}>
-        <p>{item.text}
+        <p>{item.text} - {`${item.completed}`}
         <span>
-  <FontAwesomeIcon className='faicons' icon= 'trash'
+        <FontAwesomeIcon className='faicons-check' icon= 'square-check'
+onClick={()=> props.completeItem()}/>
+
+  <FontAwesomeIcon className='faicons-trash' icon= 'trash'
   onClick={()=> props.deleteItem(item.key)}
   />
 
